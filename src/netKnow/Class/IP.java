7f3139d29ddd9ec331e2691135bddf89fe1ca25c
Kuba <jakubruszil@gmail.com>
2017-04-24 22:02:02 +0200

    obliczanie sieci

--- src/netKnow/Class/IP.java
+++ src/netKnow/Class/IP.java
@@ -3,14 +3,17 @@ package netKnow.Class;
 public class IP {
     private int [] ipArray;
     private int [] maskArray;
+    private int maskAsInteger;
     private String fullIPAdress [];
 
     public IP(String [] fullIPAdress){
         ipArray = new int[4];
         maskArray = new int[4];
+        maskAsInteger = 0;
         this.fullIPAdress = fullIPAdress;
         convertStringToIPAdress(fullIPAdress);
         convertStringToMask(fullIPAdress[4]);
+        System.out.println(computeNetwork());
     }
 
     private void convertStringToIPAdress(String [] fullIPAdress){
@@ -23,13 +26,23 @@ public class IP {
         int n = Integer.parseInt(fullIPAdress[4]);
         for(int i = 0; i < n; i++){
             maskArray[i/8] |=  (128 >> (i%8));
+            maskAsInteger++;
         }
     }
 
     private String computeNetwork(){
-
+        int [] tmp = new int[4];
+        String result = "";
+        for(int i = 0; i < 4; i++) {
+            result += (ipArray[i] & maskArray[i]) + ".";
+        }
+        return result;
     }
     private String computeBrodcast(){
-        
+        String result;
+        for(int i = 0; i < 32 - maskAsInteger; i++){
+
+        }
+        return "";
     }
 }
