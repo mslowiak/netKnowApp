399472f8045143742430e40ac6009c51fa3827dd
mq007 <marcin.slowiak.007@gmail.com>
2017-05-19 12:46:04 +0200

    Added RoutingTypeScene, RoutingTypeController and defined layout for it.

---
+++ src/netKnow/controller/RoutingTypeController.java
@@ -0,0 +1,20 @@
+package netKnow.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+
+/**
+ * Created by MQ on 2017-05-19.
+ */
+public class RoutingTypeController {
+    private Scene scene;
+
+    @FXML
+    void initialize(){
+
+    }
+
+    public void setScene(Scene scene){
+        this.scene = scene;
+    }
+}
